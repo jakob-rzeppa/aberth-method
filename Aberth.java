52bebf09e2480aad37baa984cd52a35bb1274ec0
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-03-12 20:03:06 +0100

    structure

---
+++ Aberth.java
@@ -0,0 +1,21 @@
+class Aberth {
+    /**
+     * TODO
+     * 1. Checking Polynomials / Setting Params
+     * 2. WHILE not finished
+     *  -> FOREACH Polynomial
+     *   --> get p(z_i) with z_i = current approx of root
+     *   --> get p'(z_i)
+     *   --> get p(z_i)/p'(z_i)
+     *   --> FOREACH root (var j)
+     *       --> add 1/(z_i - z_j)
+     *   --> calc nominator
+     *   --> calc denominator
+     *   --> devide nominator by denomainator
+     *   --> subtract the result from z_i and save the new root
+     *   --> print
+     *   --> check if done (for all)
+     *  -> check if done
+     * 3. Print and test
+     */
+}
